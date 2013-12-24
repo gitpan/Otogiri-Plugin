27a8c930f92b48c08e6c0e7013deec39b1aa561b
Takuya Tsuchida <tsucchi@cpan.org>
2013-12-24 01:41:37 +0000

    Import of TSUCCHI/Otogiri-Plugin-0.01 from CPAN.
    gitpan-cpan-distribution: Otogiri-Plugin
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TSUCCHI/Otogiri-Plugin-0.01.tar.gz
    gitpan-cpan-author:       TSUCCHI
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,50 @@
+[![Build Status](https://travis-ci.org/tsucchi/p5-Otogiri-Plugin.png?branch=master)](https://travis-ci.org/tsucchi/p5-Otogiri-Plugin) [![Coverage Status](https://coveralls.io/repos/tsucchi/p5-Otogiri-Plugin/badge.png?branch=master)](https://coveralls.io/r/tsucchi/p5-Otogiri-Plugin?branch=master)
+# NAME
+
+Otogiri::Plugin - make Otogiri to pluggable
+
+# SYNOPSIS
+
+    use Otogiri;
+    use Otogiri::Plugin;
+    my $db = Otogiri->new( connect_info => ["dbi:SQLite:dbname=$dbfile", '', ''] );
+    Otogiri::load_plugin('UsefulPlugin');
+    $db->useful_method; #derived from UsefulPlugin
+
+
+
+# DESCRIPTION
+
+Otogiri::Plugin provides [Teng](http://search.cpan.org/perldoc?Teng)\-like plugin function to [Otogiri](http://search.cpan.org/perldoc?Otogiri).
+
+# METHODS
+
+## $class->load\_plugin($plugin\_name, $opt)
+
+Load plugin to Otogiri or subclass. This method is exported to Otogiri or it's subclass(not to Otogiri::Plugin namespace).
+By default, plugins are loaded from Otorigi::Plugin::$plugin\_name namespace. If '+' is specified before $plugin\_name, 
+plugins are loaded specified package name. for example,
+
+    Otogiri::load_plugin('UsefulPlugin');          # loads Otogiri::Plugin::UsefulPlugin
+    Otogiri::load_plugin('+Some::Useful::Plugin'); # loads Some::Useful::Plugin
+
+You can use alias method name like this,
+
+    Otogiri::load_plugin('UsefulPlugin', { 
+        alias => {
+            very_useful_method_but_has_so_long_name => 'very_useful_method', 
+        }
+    });
+
+In this case, plugin provides `very_useful_method_but_has_so_long_name`, but you can access `very_useful_method`
+
+# LICENSE
+
+Copyright (C) Takuya Tsuchida.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Takuya Tsuchida <tsucchi@cpan.org>
---
+++ lib/Otogiri/Plugin.pm
@@ -0,0 +1,88 @@
+package Otogiri::Plugin;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+use Otogiri;
+use Module::Load;
+
+sub load_plugin { #this code is taken from Teng/SQL::Maker's load_plugin
+    my ($class, $pkg, $opt) = @_;
+    $pkg = $pkg =~ s/^\+// ? $pkg : "Otogiri::Plugin::$pkg";
+    Module::Load::load($pkg);
+
+    $class = ref($class) if ref($class);
+
+    my $alias = delete $opt->{alias} || +{};
+    {
+        no strict 'refs';
+        for my $method ( @{"${pkg}::EXPORT"} ){
+            *{$class . '::' . ($alias->{$method} || $method)} = $pkg->can($method);
+        }
+    }
+
+    $pkg->init($class, $opt) if $pkg->can('init');
+}
+
+*Otogiri::load_plugin = \&load_plugin;
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Otogiri::Plugin - make Otogiri to pluggable
+
+=head1 SYNOPSIS
+
+    use Otogiri;
+    use Otogiri::Plugin;
+    my $db = Otogiri->new( connect_info => ["dbi:SQLite:dbname=$dbfile", '', ''] );
+    Otogiri::load_plugin('UsefulPlugin');
+    $db->useful_method; #derived from UsefulPlugin
+
+
+=head1 DESCRIPTION
+
+Otogiri::Plugin provides L<Teng>-like plugin function to L<Otogiri>.
+
+=head1 METHODS
+
+=head2 $class->load_plugin($plugin_name, $opt)
+
+Load plugin to Otogiri or subclass. This method is exported to Otogiri or it's subclass(not to Otogiri::Plugin namespace).
+By default, plugins are loaded from Otorigi::Plugin::$plugin_name namespace. If '+' is specified before $plugin_name, 
+plugins are loaded specified package name. for example,
+
+    Otogiri::load_plugin('UsefulPlugin');          # loads Otogiri::Plugin::UsefulPlugin
+    Otogiri::load_plugin('+Some::Useful::Plugin'); # loads Some::Useful::Plugin
+
+You can use alias method name like this,
+
+    Otogiri::load_plugin('UsefulPlugin', { 
+        alias => {
+            very_useful_method_but_has_so_long_name => 'very_useful_method', 
+        }
+    });
+
+In this case, plugin provides C<very_useful_method_but_has_so_long_name>, but you can access C<very_useful_method>
+
+=head1 LICENSE
+
+Copyright (C) Takuya Tsuchida.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Takuya Tsuchida E<lt>tsucchi@cpan.orgE<gt>
+
+=cut
+
